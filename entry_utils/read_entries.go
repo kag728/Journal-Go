2566d55a0d7b9b077ed112e89c0eb3da732be444
jamesbury3 <jamesbury77@gmail.com>
2021-06-20 21:46:54 -0400

    Filtering entries

--- entry_utils/read_entries.go
+++ entry_utils/read_entries.go
@@ -2,18 +2,23 @@ package entry_utils
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
+// Test the supplied password on the first file in the entries folder
 func TestPassword() (bool, error) {
 	entries, err := os.ReadDir(FILE_DIR)
 	if err != nil {
 		return false, errors.Wrapf(err, "could not open directory %s", FILE_DIR)
 	}
+	entries = filter_entries(entries)
 
 	// If there aren't any entries, any password is good
 	if len(entries) == 0 {
@@ -31,7 +36,7 @@ func TestPassword() (bool, error) {
 	// Check if we can decrypt with the password
 	_, err = DecryptEntryContents(string(entry_contents))
 	if err != nil {
-		return false, nil
+		return false, errors.Wrapf(err, "error decrypting with the password")
 	}
 
 	// Password worked
@@ -45,6 +50,7 @@ func ReadEntries() error {
 	if err != nil {
 		return errors.Wrapf(err, "could not open directory %s", FILE_DIR)
 	}
+	entries = filter_entries(entries)
 
 	fmt.Printf("\n")
 	for _, entry := range entries {
@@ -63,4 +69,33 @@ func ReadEntries() error {
 		fmt.Printf("%s:\n%s\n\n", entry_name_formatted, string(decrypted_entry_contents))
 	}
 	return nil
+}
+
+// Filter list of entries so each on begins with a number and ends with a number.
+// Definitely not a great implementation but we'll see how it does
+func filter_entries(entries []fs.DirEntry) []fs.DirEntry {
+
+	filtered_entries := []fs.DirEntry{}
+	log.Infof("Original list: %v", entries)
+	for _, entry := range entries {
+
+		entry_name_split := strings.Split(entry.Name(), "_")
+
+		add_to_list := true
+		_, err := strconv.Atoi(entry_name_split[0])
+		if err != nil {
+			add_to_list = false
+		}
+
+		_, err = strconv.Atoi(entry_name_split[len(entry_name_split)-1])
+		if err != nil {
+			add_to_list = false
+		}
+
+		if add_to_list {
+			filtered_entries = append(filtered_entries, entry)
+		}
+	}
+	log.Infof("Filtered list: %v", filtered_entries)
+	return filtered_entries
 }
