303cc54b5adaf1b3711f217b85e75eeec9191820
jamesbury3 <jamesbury77@gmail.com>
2021-07-07 09:21:36 -0400

    Moved uploader code to pkg

--- cmd/uploader/main.go
+++ cmd/uploader/main.go
@@ -1,67 +1,18 @@
 package main
 
 import (
-	"io/ioutil"
-	"journal/pkg/entry_utils"
-	"os"
-	"path"
-	"strings"
+	"journal/pkg/uploader"
 
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
-
-const cloud_config = ".internal/cloudconfig"
 
 func main() {
 	log.Info("Uploading entries to cloud.")
 
-	cloud_dir, err := get_cloud_dir()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	entry_name, err := upload(cloud_dir)
+	entry_name, err := uploader.Upload()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Infof("Successfully uploaded %s", entry_name)
-}
-
-func get_cloud_dir() (string, error) {
-	cloud_dir, err := os.ReadFile(cloud_config)
-	if err != nil {
-		return "", errors.Wrapf(err, "error reading contents of cloud config file")
-	}
-	return strings.TrimSuffix(string(cloud_dir), "\n"), nil
-}
-
-func upload(cloud_dir string) (string, error) {
-	entry, err := entry_utils.GetCurrentEntry()
-	if err != nil {
-		return "", errors.Wrapf(err, "error getting current entry")
-	}
-
-	entry_contents, err := ioutil.ReadFile(entry.Name())
-	if err != nil {
-		return "", errors.Wrapf(err, "could not read editor contents")
-	}
-	defer entry.Close()
-
-	entry_name := path.Join(
-		cloud_dir,
-		strings.Split(entry.Name(), "/")[1],
-	)
-	_, err = os.Create(entry_name)
-	if err != nil {
-		return "", errors.Wrapf(err, "error creating entry in cloud folder")
-	}
-
-	err = ioutil.WriteFile(entry_name, entry_contents, 7777)
-	if err != nil {
-		return "", errors.Wrapf(err, "error writing contents to cloud entry")
-	}
-
-	return entry_name, nil
 }
---
+++ pkg/uploader/upload.go
@@ -0,0 +1,57 @@
+package uploader
+
+import (
+	"io/ioutil"
+	"journal/pkg/entry_utils"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const cloud_config = ".internal/cloudconfig"
+
+// Uploads today's entry to the folder specified in the cloud_config file.
+func Upload() (string, error) {
+
+	cloud_dir, err := get_cloud_dir()
+	if err != nil {
+		return "", errors.Wrapf(err, "error getting cloud directory")
+	}
+
+	entry, err := entry_utils.GetCurrentEntry()
+	if err != nil {
+		return "", errors.Wrapf(err, "error getting current entry")
+	}
+
+	entry_contents, err := ioutil.ReadFile(entry.Name())
+	if err != nil {
+		return "", errors.Wrapf(err, "could not read editor contents")
+	}
+	defer entry.Close()
+
+	entry_name := path.Join(
+		cloud_dir,
+		strings.Split(entry.Name(), "/")[1],
+	)
+	_, err = os.Create(entry_name)
+	if err != nil {
+		return "", errors.Wrapf(err, "error creating entry in cloud folder")
+	}
+
+	err = ioutil.WriteFile(entry_name, entry_contents, 7777)
+	if err != nil {
+		return "", errors.Wrapf(err, "error writing contents to cloud entry")
+	}
+
+	return entry_name, nil
+}
+
+func get_cloud_dir() (string, error) {
+	cloud_dir, err := os.ReadFile(cloud_config)
+	if err != nil {
+		return "", errors.Wrapf(err, "error reading contents of cloud config file")
+	}
+	return strings.TrimSuffix(string(cloud_dir), "\n"), nil
+}
